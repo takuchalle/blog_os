7b3341e36f9367e916eaa2838fd57bed2c3b2587
takuchalle <me@takuchalle.dev>
2020-12-14 15:57:07 +0900

    Add println macro

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,15 @@ use core::panic::PanicInfo;
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello world{}", "!");
 
     loop {}
 }
+
