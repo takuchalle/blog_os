07dba025d1e64d0549579764f9197e33230bad9c
takuchalle <me@takuchalle.dev>
2020-12-15 12:38:40 +0900

    Define custom test frameworks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga_buffer;
 
@@ -20,3 +22,10 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
