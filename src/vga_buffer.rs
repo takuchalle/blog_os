30cec309d6059f48b154f1dd42a8ec659d511221
takuchalle <me@takuchalle.dev>
2020-12-15 20:12:56 +0900

    Support blank

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -99,7 +99,7 @@ impl Writer {
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
-                0x21..=0x7e | b'\n' => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 _ => self.write_byte(0xfe),
             }
         }
