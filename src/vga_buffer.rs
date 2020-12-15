4ca59f48f5678e891bcb6a700e3031e26210237c
takuchalle <me@takuchalle.dev>
2020-12-15 12:35:58 +0900

    cargo fmt

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,13 +1,13 @@
 use core::fmt;
-use volatile::Volatile;
 use lazy_static::lazy_static;
 use spin::Mutex;
+use volatile::Volatile;
 
 lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer{
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer)  },
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 }
 
