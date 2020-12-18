2f2e586b94d4feecbf95c593ff91f2da841bb203
takuchalle <me@takuchalle.dev>
2020-12-18 11:38:32 +0900

    Use SerialPort to print

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 mod serial;
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -45,7 +45,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -55,7 +55,7 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
