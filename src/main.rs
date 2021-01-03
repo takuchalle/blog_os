2b17b7660b846e6f0d43681802efaed115352392
takuchalle <me@takuchalle.dev>
2021-01-03 17:56:43 +0900

    feat(memory): use OffsetPageTable

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 use blog_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::PageTable;
 
 /// This function is called on panic.
 #[cfg(not(test))]
@@ -26,10 +27,28 @@ fn panic(_info: &PanicInfo) -> ! {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello world{}", "!");
+    use blog_os::memory;
+    use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
 
+    println!("Hello world{}", "!");
     blog_os::init();
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        0xb8000,
+        0x201008,
+        0x0100_0020_1a10,
+        0x110,
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
     #[cfg(test)]
     test_main();
 
