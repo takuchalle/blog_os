94ad98802effc43e31500ab9978854dc9f15ba5b
takuchalle <me@takuchalle.dev>
2021-01-07 11:33:19 +0900

    feat(allocator): use linked_list_allocator instead of dummy allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,5 +1,4 @@
-use alloc::alloc::{GlobalAlloc, Layout};
-use core::ptr::null_mut;
+use linked_list_allocator::LockedHeap;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -10,20 +9,8 @@ use x86_64::{
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 0x100 * 1024;
 
-pub struct Dummy;
-
 #[global_allocator]
-static ALLOCATOR: Dummy = Dummy;
-
-unsafe impl GlobalAlloc for Dummy {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
-    }
-
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
-        panic!("dealloc should be never called")
-    }
-}
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
@@ -43,6 +30,10 @@ pub fn init_heap(
             .ok_or(MapToError::FrameAllocationFailed)?;
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
         unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
+    }
+
+    unsafe {
+        ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
     }
     Ok(())
 }
