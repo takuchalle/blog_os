80bcb8e5dff242dbaaaede6beb41801dfc748af2
takuchalle <me@takuchalle.dev>
2021-01-09 18:25:32 +0900

    feat(allocator): add BumpAllocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,4 +1,6 @@
-use linked_list_allocator::LockedHeap;
+pub mod bump;
+
+use bump::BumpAllocator;
 use x86_64::{
     structures::paging::{
         mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
@@ -10,7 +12,27 @@ pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 0x100 * 1024;
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
