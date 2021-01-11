2682bf1a1c9899a34aab112919b37cf1f7c9c137
takuchalle <me@takuchalle.dev>
2021-01-11 12:47:40 +0900

    feat(allocator): use linked_list_allocator crate

--- src/allocator.rs
+++ src/allocator.rs
@@ -8,12 +8,13 @@ use x86_64::{
     },
     VirtAddr,
 };
+use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 0x100 * 1024;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
