090a44e1f3fc56b6f3f40bec6d775074dfad27fa
takuchalle <me@takuchalle.dev>
2021-01-21 22:52:41 +0900

    feat(task): add executor

---
+++ src/task/executor.rs
@@ -0,0 +1,28 @@
+use super::{Task, TaskId};
+use alloc::{collections::BTreeMap, sync::Arc};
+use core::task::Waker;
+use crossbeam_queue::ArrayQueue;
+
+pub struct Executor {
+    tasks: BTreeMap<TaskId, Task>,
+    task_queue: Arc<ArrayQueue<TaskId>>,
+    waker_cache: BTreeMap<TaskId, Waker>,
+}
+
+impl Executor {
+    pub fn new() -> Self {
+        Executor {
+            tasks: BTreeMap::new(),
+            task_queue: Arc::new(ArrayQueue::new(100)),
+            waker_cache: BTreeMap::new(),
+        }
+    }
+
+    pub fn spawn(&mut self, task: Task) {
+        let task_id = task.id;
+        if self.tasks.insert(task.id, task).is_some() {
+            panic!("task with same ID already in tasks");
+        }
+        self.task_queue.push(task_id).expect("queue fulle");
+    }
+}
